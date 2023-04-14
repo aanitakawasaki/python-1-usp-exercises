c77d5ccccf9e8ca1ad8ccf59b49969cdabfdf833
Anita Yumi Kawasaki <aanitakawasaki@gmail.com>
2023-04-14 09:57:30 -0300

    updating  exercise files in GitHub

--- semana 6/jogo_do_nim3.py
+++ semana 6/jogo_do_nim3.py
@@ -21,32 +21,44 @@ def partida ():
 
     if n % (m + 1) == 0:
         print ('Você começa!')
-        #>>>
+
+        while n > 0:
+            '''aqui eu não preciso chamar a função usuario_escolhe_jogada (n, m) e depois atribuir
+            usuario_escolhe_jogada (n, m) à pecasRetiradas, como eu estava fazendo, só fazer essa atribuição,
+            como abaixo, já basta, afinal*'''
+            pecasRetiradas = usuario_escolhe_jogada (n, m)
+            print ('Você tirou', pecasRetiradas, 'peça(s)')
+            n = n - pecasRetiradas
+            print ('Agora restam', n, 'peça(s) no tabuleiro')
+            usuario_joga = False
+
+            pecasRetiradas = computador_escolhe_jogada (n, m)
+            print ('O computador retirou', pecasRetiradas, 'peça(s)')
+            n = n - pecasRetiradas
+            print ('Agora restam', n, 'peça(s) no tabuleiro')
+            usuario_joga = True
+
+        if n == 0 and usuario_joga:
+            print ('Fim do jogo! O computador ganhou!')
+        
     else:
         print ('Computador começa!')
-        #>>>
-        
-    while n != 0:
-        #>>>
-        '''aqui eu não preciso chamar a função usuario_escolhe_jogada (n, m) e depois atribuir
-        usuario_escolhe_jogada (n, m) à pecasRetiradas, como eu estava fazendo, só fazer essa atribuição,
-        como abaixo, já basta, afinal*'''
-        pecasRetiradas = usuario_escolhe_jogada (n, m)
-        print ('Você tirou', pecasRetiradas, 'peça(s)')
-        n = n - pecasRetiradas
-        print ('Agora restam', n, 'peça(s) no tabuleiro')
-        usuario_joga = False
 
-        #>>>
-        pecasRetiradas = computador_escolhe_jogada (n, m)
-        print ('O computador retirou', pecasRetiradas, 'peça(s)')
-        n = n - pecasRetiradas
-        print ('Agora restam', n, 'peça(s) no tabuleiro')
-        usuario_joga = True
+        while n > 0:
+            pecasRetiradas = computador_escolhe_jogada (n, m)
+            print ('Computador tirou', pecasRetiradas, 'peça(s)')
+            n = n - pecasRetiradas
+            print ('Agora restam', n , 'peça(s) no tabuleiro')
+            usuario_joga = True
+            
+            pecasRetiradas = usuario_escolhe_jogada (n, m)
+            print ('Você tirou', pecasRetiradas, 'peça(s)')
+            n = n - pecasRetiradas
+            print ('Agora restam', n, 'peça(s) no tabuleiro')
+            usuario_joga = False
 
-    if n == 0 and usuario_joga:
-        print ('Fim do jogo! O computador ganhou!')
-
+        if n == 0 and not usuario_joga:
+            print ('Fim do jogo! Você ganhou!')
 
 
 def usuario_escolhe_jogada (n, m):
@@ -84,6 +96,6 @@ O erro está em partes do seu código que são assim:
     usuario_escolhe_jogada (n, m)
     n = n - (usuario_escolhe_jogada (n, m))
 Uma função não é uma variável: toda vez que você chama ela, o código inteiro dentro dela é executado. Nesse caso
-acima você chama a função com valores n e m, e não guarda seu retorno em nenhum lugar. Em baixo você chama de novo
+acima você chama a função com valores n e m, e não guarda seu retorno em nenhum lugar. Embaixo você chama de novo
 mas ai você usa o valor de retorno da função.
 '''
