7fdbb2c1a38405fe4bd817e70aba210ff43ff2d5
Anita Yumi Kawasaki <aanitakawasaki@gmail.com>
2023-04-14 10:34:34 -0300

    updating  exercise files in GitHub

--- semana 6/jogo_do_nim3.py
+++ semana 6/jogo_do_nim3.py
@@ -98,6 +98,76 @@ def computador_escolhe_jogada (n, m):
 
 
 
+def campeonato ():
+    controle = 1
+    while controle <= 3:
+        print ('*** Rodada', controle, ' ***')
+        
+        n = int (input ("Quantas peças? "))
+        while n < 1:
+            print ("Você deve colocar no mínimo 1 peça no tabuleiro!")
+            n = int (input ("Quantas peças? "))
+        
+        m = int (input ("Limite de peças por jogada? "))
+        while m < 1 or m > n:
+            print ("Você deve permitir a retirada de pelo menos 1 peça do tabuleiro. E não pode permitir a retirada de mais peças do que a quantidade total no tabuleiro")
+            m = int (input ("Limite de peças por jogada? "))
+        
+        if n % (m + 1) == 0:
+            print ('Você começa!')
+                        
+            while n > 0:
+                if n > 0:
+                    '''aqui eu não preciso chamar a função usuario_escolhe_jogada (n, m) e depois atribuir
+                    usuario_escolhe_jogada (n, m) à pecasRetiradas, como eu estava fazendo, só fazer essa atribuição,
+                    como abaixo, já basta, afinal*'''
+                    pecasRetiradas = usuario_escolhe_jogada (n, m)
+                    print ('Você tirou', pecasRetiradas, 'peça(s)')
+                    n = n - pecasRetiradas
+                    print ('Agora restam', n, 'peça(s) no tabuleiro')
+                    usuario_jogou = True
+                if n == 0 and usuario_jogou:
+                    print ('Fim do jogo! Você ganhou!')
+            
+                if n > 0:
+                    pecasRetiradas = computador_escolhe_jogada (n, m)
+                    print ('O computador retirou', pecasRetiradas, 'peça(s)')
+                    n = n - pecasRetiradas
+                    print ('Agora restam', n, 'peça(s) no tabuleiro')
+                    usuario_jogou = False
+                if n == 0 and not usuario_jogou:
+                    print ('Fim do jogo! O computador ganhou!')
+                            
+        else:
+            print ('Computador começa!')
+            
+            while n > 0:
+                if n > 0:
+                    pecasRetiradas = computador_escolhe_jogada (n, m)
+                    print ('Computador tirou', pecasRetiradas, 'peça(s)')
+                    n = n - pecasRetiradas
+                    print ('Agora restam', n , 'peça(s) no tabuleiro')
+                    computador_jogou = True
+                if n == 0 and computador_jogou:
+                    print ('Fim do jogo! O computador ganhou!')
+            
+                if n > 0:
+                    pecasRetiradas = usuario_escolhe_jogada (n, m)
+                    print ('Você tirou', pecasRetiradas, 'peça(s)')
+                    n = n - pecasRetiradas
+                    print ('Agora restam', n, 'peça(s) no tabuleiro')
+                    computador_jogou = False
+                if n == 0 and not computador_jogou:
+                    print ('Fim do jogo! Você ganhou!')
+            
+        controle = controle + 1
+
+    print ('*** Final do campeonato! ***')
+
+    #falta o "Placar: Você 0 X 3 Computador"
+
+
+
 partida ()
 
 
