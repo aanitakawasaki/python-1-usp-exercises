43b7d825d6601cbf627efc1e70a71411331bd1ec
Anita Yumi Kawasaki <aanitakawasaki@gmail.com>
2023-05-04 10:22:59 -0300

    updating  exercise files in GitHub

--- semana 7/quantos-primos.py
+++ semana 7/quantos-primos.py
@@ -15,19 +15,14 @@ def ePrimo (n):
 
 
 
-#recebe o número:
-numeroDigitado = int (input ("digite um número maior ou igual a 2: "))
-while numeroDigitado < 2:
-    numeroDigitado = int (input ("digite um número maior ou igual a 2: "))
-
-#vai passando de numeroDigitado até 2 (subtraindo de 1 em 1):
-numeroAtual = numeroDigitado
-quantidadePrimos = 0
-while numeroAtual >= 2:
-    #vai verificando se cada um desses "numeroAtual" é primo
-    if ePrimo (numeroAtual):
-        quantidadePrimos = quantidadePrimos + 1    
-    numeroAtual = numeroAtual - 1
-    
-#printa a quantidade de números primos dentro daquele número:
-print (quantidadePrimos)
+def n_primos (n):
+    #vai passando de numeroDigitado até 2 (subtraindo de 1 em 1):
+    numeroAtual = n
+    quantidadePrimos = 0
+    while numeroAtual >= 2:
+        #vai verificando se cada um desses "numeroAtual" é primo
+        if ePrimo (numeroAtual):
+            quantidadePrimos = quantidadePrimos + 1    
+        numeroAtual = numeroAtual - 1
+        
+    return quantidadePrimos
