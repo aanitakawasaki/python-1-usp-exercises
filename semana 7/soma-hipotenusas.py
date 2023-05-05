9f85c83b46869b866164bdc35b2ed432597c42f0
Anita Yumi Kawasaki <aanitakawasaki@gmail.com>
2023-05-05 11:17:45 -0300

    updating  exercise files in GitHub

---
+++ semana 7/soma-hipotenusas.py
@@ -0,0 +1,20 @@
+#relembrando o conceito: uma hipotenusa é o lado maior de um triângulo retângulo (o lado oposto ao ângulo reto)
+# hip² = catA² + catB² (sendo catA cateto A e catB cateto B)
+# 10² = 6² + 8² (hip = 10; catA = 6; catB = 8)
+
+def é_hipotenusa (n):
+
+    
+
+def soma_hipotenusas (n):
+
+
+    
+numeroDigitado = (int (input ("digite um número inteiro positivo: ")))
+while numeroDigitado <= 0:
+    numeroDigitado = (int (input ("digite um número inteiro positivo: ")))
+
+numeroAtual = 1
+while numeroAtual <= numeroDigitado:
+    é_hipotenusa (numeroAtual)
+    numeroAtual = numeroAtual + 1
