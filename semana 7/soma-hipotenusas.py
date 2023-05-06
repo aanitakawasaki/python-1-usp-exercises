ae46f0cec742fc50b22fc80cb6964c3ab0c3fbc5
Anita Yumi Kawasaki <aanitakawasaki@gmail.com>
2023-05-06 16:28:12 -0300

    updating  exercise files in GitHub

--- semana 7/soma-hipotenusas.py
+++ semana 7/soma-hipotenusas.py
@@ -3,14 +3,15 @@
 # 10² = 6² + 8² (hip = 10; catA = 6; catB = 8)
 
 def é_hipotenusa (n):
-    #sendo n = 1 #sendo n = 2
-    catetoA = n #1 #2
-    while catetoA <= n: #True #False
-        catetoB = 1 #1
-        while catetoB <= n: #True #False
-            print (catetoA ** 2, "+", catetoB ** 2, "=", catetoA **2 + catetoB ** 2) #1**2 + 1**2 = 2
-            catetoB = catetoB + 1 #2
-        catetoA = catetoA + 1 #2
+    catetoA = 1
+    while catetoA <= n: 
+        catetoB = 1 
+        while catetoB <= n: 
+            if catetoA ** 2 + catetoB ** 2 == n ** 2:
+                print (catetoA ** 2, "+", catetoB ** 2, "=", catetoA ** 2 + catetoB ** 2)
+                print ("hipotenusa")
+            catetoB = catetoB + 1 
+        catetoA = catetoA + 1 
 
 #def soma_hipotenusas (n):
 
@@ -19,9 +20,5 @@ def é_hipotenusa (n):
 numeroDigitado = (int (input ("digite um número inteiro positivo: ")))
 while numeroDigitado <= 0:
     numeroDigitado = (int (input ("digite um número inteiro positivo: ")))
-    #sendo numeroDigitado = 10
 
-numeroAtual = 1 #1
-while numeroAtual <= numeroDigitado: #1 <= 10 True #2 <= 10 True
-    é_hipotenusa (numeroAtual) #é_hipotenusa (1) #é_hipotenusa (2)
-    numeroAtual = numeroAtual + 1 #numeroAtual = 1 + 1 = 2
+é_hipotenusa (numeroDigitado)
